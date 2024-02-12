8495d171d17883b15936f9c0bd60bcedd681c5fc
Geetanjali2301 <geetanjalipohare@gmail.com>
2024-02-12 09:29:54 +0530

    Binary Trees

---
+++ operations_tree.java
@@ -0,0 +1,74 @@
+public class operations_tree {
+
+    class node{
+        int data;
+        node right;
+        node left;
+
+        public node(int data) {
+            this.data=data;
+            this.left=null;
+            this.right=null;
+            
+        }
+    }
+
+
+
+    //height of tree
+    public int height(node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        int ht=Math.max(lh,rh)+1;
+        return ht;
+
+    }
+
+    //count of total number of nodes in the tree
+    public int count(node root)
+    {
+        
+        if(root==null)
+        {
+            return 0;
+        }
+        int left=count(root.left);
+        int right=count(root.right);
+        return left+right+1;
+
+    }
+
+    //sum of all nodes of a tree
+    public int sum(node root)
+    {
+        
+        if(root==null)
+        {
+            return 0;
+        }
+        int leftsum=sum(root.left);
+        int rightsum=sum(root.right);
+        return leftsum+rightsum+root.data;
+
+    }
+  
+    public static void main(String[] args) {
+        operations_tree t=new operations_tree();
+        node root=t.new node(10);
+        root.left=t.new node(20);
+        root.right=t.new node(30);
+        root.left.left=t.new node(12);
+        root.left.left.left=t.new node(13);
+        System.out.println("Height of tree is: "+t.height(root));
+        System.out.println("Num of nodes of tree is: "+t.count(root));
+        System.out.println("Sum of nodes of tree is: "+t.sum(root));
+       
+
+    }
+    
+}
